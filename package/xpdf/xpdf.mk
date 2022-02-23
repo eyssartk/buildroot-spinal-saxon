4c6175ac527c92bee6db57ad2841cfefc77df15b
Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
2022-02-23 15:21:38 +0100

    Update xpdf.mk

--- package/xpdf/xpdf.mk
+++ package/xpdf/xpdf.mk
@@ -14,6 +14,7 @@ XPDF_CONF_OPTS = -DCMAKE_VERBOSE_MAKEFILE=ON
 # XPDF_DEPENDENCIES = libglib2 host-pkgconf
 XPDF_DEPENDENCIES = freetype qt5base
 
+# Bug g++: use another riscv-toolchain
 # cd /home/eyssartk/Dev/ArtyA7SmpLinux/buildroot-build/build/xpdf-4.03/goo && /opt/riscv32/bin/riscv32-unknown-linux-gnu-g++  --sysroot=/home/eyssartk/Dev/ArtyA7SmpLinux/buildroot-build/per-package/xpdf/host/riscv32-buildroot-linux-gnu/sysroot   -I/home/eyssartk/Dev/ArtyA7SmpLinux/buildroot-build/build/xpdf-4.03  -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -Os -DSYS_futex=SYS_futex_time64 -D__NR_futex=__NR_futex_time64 -O3 -Wall -fPIC   -o CMakeFiles/goo_objs.dir/GString.cc.o -c /home/eyssartk/Dev/ArtyA7SmpLinux/buildroot-build/build/xpdf-4.03/goo/GString.cc
 
 $(eval $(cmake-package))
